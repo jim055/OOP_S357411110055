ba9c96526126b88a0bff225ce6eb676ebb5d7124
jim055 <thasawan_13@hotmail.com>
2017-11-19 14:55:27 +0700

    Testing upload

---
+++ src/oop_lab/s357411110055/rmutsv/com/HelloWorid.java
@@ -0,0 +1,15 @@
+package oop_lab.s357411110055.rmutsv.com;
+
+public class HelloWorid {
+
+    // create main_method
+    public static void main(String[] args) {
+        System.out.print("Hello World,I'am SAM");
+
+
+    }//main
+
+
+
+
+}//class
