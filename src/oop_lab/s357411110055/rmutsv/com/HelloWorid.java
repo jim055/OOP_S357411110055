115bdcfcaafad850a4ee564eb6d1927a7fe86b8d
jim055 <thasawan_13@hotmail.com>
2017-11-19 15:21:29 +0700

    Add informaton

--- src/oop_lab/s357411110055/rmutsv/com/HelloWorid.java
+++ src/oop_lab/s357411110055/rmutsv/com/HelloWorid.java
@@ -4,7 +4,10 @@ public class HelloWorid {
 
     // create main_method
     public static void main(String[] args) {
-        System.out.print("Hello World,I'am SAM");
+        System.out.print("Hello World,I'am Jim");
+        System.out.print("Puriwat Lertkrai");
+        System.out.print("Information System");
+        System.out.print("MT: RMUTSV");
 
 
     }//main
