92d62a142ad5e41dddad35b392230de1b24b69b6
jim <thasawan_13@hotmail.com>
2017-12-12 11:16:38 +0700

    Test Switch case

---
+++ src/opp_lab3/TestSwitchCase.java
@@ -0,0 +1,37 @@
+package opp_lab3;
+public class TestSwitchCase {
+    public static void main (String[] args){
+//      switch-case
+            BufferedReader reader = new  BufferedReader(
+                    new TnputStreamReader(System.in));
+            int input;
+            System.out.print("Plese enter number (1-3): ");
+            input = Integer.parseInt(reader.readLine())ว
+            switch (input)   {
+                case 1: System.out.printIn("Your entered 1.");break;
+                case 2: System.out.printIn("Your entered 2.");break;
+                case 3: System.out.printIn("Your entered 3.");break;
+                default: System.out.printIn("Please enter number 1-3.");
+
+
+
+            } //switch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+}//class
---
+++ src/opp_lab3/TestWhileLoop.java
@@ -0,0 +1,25 @@
+package opp_lab3;
+
+public class TestWhileLoop {
+
+    public static void main(String [] args){
+//      while
+        int i = 1;
+        while (i<=10){
+            System.out.print(i+"Hello
+            i++;
+        }//while
+//      do-while
+            int j=1;
+            do {
+                System.out.print(j + "\n ");
+                j++;
+            }while (j <= 10);
+
+
+
+
+    }//main
+
+
+}//class
