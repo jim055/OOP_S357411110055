9a02209b33bc94c77976fc3d2b802ce8604e3f7e
jim055 <thasawan_13@hotmail.com>
2017-12-27 17:59:05 +0700

    string

--- src/oop_lab6/StringCompare.java
+++ src/oop_lab6/StringCompare.java
@@ -1,4 +1,27 @@
 package oop_lab6;
 
 public class StringCompare {
-}
+    public static void main (String [] args){
+ //     compare String
+        String str1 = "Hello";
+        String str2 = "Hellooo";
+        // type 1 (==) true,false
+        if (str1==str2) System.out.println("true");
+        else System.out.println("false");
+        // type 2 (equals() method)
+        if (str1.equals(str2))System.out.println("true");
+        else System.out.println("false");
+        // type 3 (compareTo() method)
+        // -,+,0
+        if (str1.compareTo(str2))
+            System.out.println("true");
+        else if (str1.compareTo(str2)>=1)
+            System.out.println("str 2 more than str1");
+        else
+            System.out.println("strl else than str2");
+
+
+
+    }//main
+
+}//class
